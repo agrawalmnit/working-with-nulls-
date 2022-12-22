c09fb0200b5d85b90b73c4beacc5d9327e813ea3
ishansi-abjima <119054363+ishansi-abjima@users.noreply.github.com>
2022-12-22 15:37:40 +0530

    Add files via upload

---
+++ ConsoleApp30/ConsoleApp6/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace 
+    
+    ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var player = new PlayerCharacter();
+            player.Name = "";
+            player.DaysSinceLastLogin = 42;
+
+            PlayerDisplayer.Write(player);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp30/ConsoleApp7/PlayerCharacter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class PlayerCharacter
+    {
+        public string Name { get; set; }
+        public int? DaysSinceLastLogin { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public bool? IsNew { get; set; }
+    }
+}
